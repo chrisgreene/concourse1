59fa49397ae40049e56f534eb3d3d1cde8fad60c
Chris Greene <chrisgreene@gmail.com>
2018-12-17 12:40:56 -0700

    testing

--- multi-job/update-file.sh
+++ multi-job/update-file.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 #mkdir results
-echo 'one' >> concourse1/appendme
-echo 'two' >> concourse1/appendme
+ls -ltR
+#echo 'one' >> concourse1/appendme
+#echo 'two' >> concourse1/appendme
 
-cat concourse1/appendme
+#cat concourse1/appendme
